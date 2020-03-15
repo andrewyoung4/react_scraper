1c77f652df8456cb1d0e9b6d73e8ade0bb98d605
Drew <andrewyoung4@mail.weber.edu>
2020-03-14 21:58:10 -0600

    adding final product

---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer>
+      <hr />
+      <p className="pull-right">
+        <i className="fab fa-github" /> Drew Young 2020
+      </p>
+    </footer>
+  );
+}
+
+export default Footer;
